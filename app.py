e377020f7cedd411cbf1e469fc53c170dfdbd30e
Shimolee Jhaveri <shimoleejhaveri@gmail.com>
2020-04-27 07:01:11 +0000

    Added JSON file and created a Flask route

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+@app.route('/dynamicsay', methods=['POST'])
+def dynamic_say():
+    return send_file('dynamicsay.json')
