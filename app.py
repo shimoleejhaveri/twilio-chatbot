990ebb054caa7bd2c98f38f0367d38a51061791f
Shimolee Jhaveri <shimoleejhaveri@gmail.com>
2020-04-27 21:46:57 +0000

    Added more routes and updated JSON file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_file
+import json
+from flask import Flask, send_file, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,6 +10,23 @@ def home():
 @app.route('/dynamicsay', methods=['POST'])
 def dynamic_say():
     return send_file('dynamicsay.json')
+
+@app.route('/collect',  methods=['POST'])
+def collect():
+    memory = json.loads(request.form.get('Memory'))
+
+    answers = memory['twilio']['collected_data']['collect_clothes_order']['answers']
+
+    first_name = answers['first_name']['answer']
+    clothes_type = answers['clothes_type']['answer']
+    num_clothes = answers['num_clothes']['answer']
+
+    message = (
+        f'Ok {first_name}. Your order for {num_clothes} {clothes_type} is now confirmed.'
+        f' Thank you for ordering with us'
+    )
+
+    return jsonify(actions=[{'say': {'speech': message}}])
 
 if __name__ == "__main__":
     app.run()
